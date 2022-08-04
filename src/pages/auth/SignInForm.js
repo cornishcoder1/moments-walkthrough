f6f8ed966ce48c4615eeafb1f69f3b461ebef10c
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-08-04 09:17:00 +0000

    Use useContext hook to check who is logged in and what icons should be displayed in the navbar

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import axios from "axios";
 
 import Form from "react-bootstrap/Form";
@@ -14,33 +14,38 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
 
-function SignInForm () {
-    const [signInData, setSignInData] = useState({
-      username: "",
-      password: "",
-    });
-    const { username, password } = signInData;
+function SignInForm() {
+  const setCurrentUser = useContext(SetCurrentUserContext);
 
-    const [errors, setErrors] = useState({});
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: "",
+  });
+  const { username, password } = signInData;
 
-    const history = useHistory();
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            await axios.post("/dj-rest-auth/login/", signInData);
-            history.push("/");
-        } catch (err) {
-            setErrors(err.response?.data);
-        }
-    };
+  const [errors, setErrors] = useState({});
 
-const handleChange = (event) => {
+  const history = useHistory();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  const handleChange = (event) => {
     setSignInData({
-        ...signInData,
-        [event.target.name]: event.target.value,
+      ...signInData,
+      [event.target.name]: event.target.value,
     });
-};
+  };
 
   return (
     <Row className={styles.Row}>
@@ -51,26 +56,27 @@ const handleChange = (event) => {
             <Form.Group controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
-                className={styles.Input}
                 type="text"
                 placeholder="Username"
                 name="username"
+                className={styles.Input}
                 value={username}
                 onChange={handleChange}
               />
             </Form.Group>
             {errors.username?.map((message, idx) => (
-              <Alert key={idx}>
+              <Alert key={idx} variant="warning">
                 {message}
               </Alert>
             ))}
+
             <Form.Group controlId="password">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
-                className={styles.Input}
                 type="password"
                 placeholder="Password"
                 name="password"
+                className={styles.Input}
                 value={password}
                 onChange={handleChange}
               />
@@ -80,18 +86,18 @@ const handleChange = (event) => {
                 {message}
               </Alert>
             ))}
-            <Button 
-            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} 
-            type="submit">
-                Sign In
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign in
             </Button>
             {errors.non_field_errors?.map((message, idx) => (
               <Alert key={idx} variant="warning" className="mt-3">
                 {message}
               </Alert>
             ))}
-          </Form>  
-
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
