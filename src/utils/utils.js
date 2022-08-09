ad4359ab1a1e3be5ad486600393316e453b36da9
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-08-09 07:58:58 +0000

    Add search bar and install infinite scroll

---
+++ src/utils/utils.js
@@ -0,0 +1,16 @@
+import { axiosReq } from "../api/axiosDefaults";
+
+export const fetchMoreData = async (resource, setResource) => {
+    try {
+        const { data } = await axiosReq.get(resource.next);
+        setResource((prevResource) => ({
+            ...prevResource,
+            next: data.next,
+            results: data.results.reduce((acc, curr) => {
+                return acc.some((accResult) => accResult.id === curr.id)
+                    ? acc
+                    : [...acc, curr];      
+            }, prevResource.results),
+        }));
+    } catch (err) {}
+};
