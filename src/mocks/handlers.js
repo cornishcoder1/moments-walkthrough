00362c4b4f3b036561d5f34ae8704f9801441a98
cornishcoder1 <leahfisher07@hotmail.co.uk>
2022-08-15 13:05:37 +0000

    Setup Jest testing

---
+++ src/mocks/handlers.js
@@ -0,0 +1,19 @@
+const baseURL = "https://first-drf-api.herokuapp.com";
+
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(ctx.json({
+            pk: 5,
+            username: "John",
+            email: "",
+            first_name: "",
+            last_name: "",
+            profile_id: 5,
+            profile_image: "https://res.cloudinary.com/dexhos525/image/upload/v1/media/images/sushi_pimc2t"
+            })
+        );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+];
